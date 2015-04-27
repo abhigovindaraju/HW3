66f1f6aab4662f9d6cfff1844f4c435157298c61
abhigovindaraju <abhigovindaraju@gmail.com>
2015-04-26 19:50:41 -0700

    Update uart.c

--- uart.c
+++ uart.c
@@ -37,22 +37,22 @@ void UART0_Init(int baudrate)
 	unsigned long int Fdiv;
 
 	// PCLK_UART0 is being set to 1/4 of SystemCoreClock
-	pclk = SystemCoreClock / 4;	
-	
+	pclk = SystemCoreClock / 4;
+
 	// Turn on power to UART0
 	LPC_SC->PCONP |=  PCUART0_POWERON;
-		
+
 	// Turn on UART0 peripheral clock
 	LPC_SC->PCLKSEL0 &= ~(PCLK_UART0_MASK);
 	LPC_SC->PCLKSEL0 |=  (0 << PCLK_UART0);		// PCLK_periph = CCLK/4
-	
+
 	// Set PINSEL0 so that P0.2 = TXD0, P0.3 = RXD0
 	LPC_PINCON->PINSEL0 &= ~0xf0;
 	LPC_PINCON->PINSEL0 |= ((1 << 4) | (1 << 6));
-	
+
 	LPC_UART0->LCR = 0x83;		// 8 bits, no Parity, 1 Stop bit, DLAB=1
     Fdiv = ( pclk / 16 ) / baudrate ;	// Set baud rate
-    LPC_UART0->DLM = Fdiv / 256;							
+    LPC_UART0->DLM = Fdiv / 256;
     LPC_UART0->DLL = Fdiv % 256;
     LPC_UART0->LCR = 0x03;		// 8 bits, no Parity, 1 Stop bit DLAB = 0
     LPC_UART0->FCR = 0x07;		// Enable and reset TX and RX FIFO
@@ -93,7 +93,7 @@ void UART1_Init(int baudrate)
 void UART0_Sendchar(char c)
 {
 	while( (LPC_UART0->LSR & LSR_THRE) == 0 );	// Block until tx empty
-	
+
 	LPC_UART0->THR = c;
 }
 
@@ -109,7 +109,7 @@ void UART1_Sendchar(char c)
 char UART0_Getchar()
 {
 	char c;
-	while( (LPC_UART0->LSR & LSR_RDR) == 0 );  // Nothing received so just block 	
+	while( (LPC_UART0->LSR & LSR_RDR) == 0 );  // Nothing received so just block
 	c = LPC_UART0->RBR; // Read Receiver buffer register
 	return c;
 }
@@ -148,3 +148,29 @@ void UART1_PrintString(char *pcString)
 }
 
 
+int main(void)
+{
+
+	uint8_t databyte = 0;
+		UART1_Init(19200);
+		UART1_PrintString("==================================\r\n");
+		UART1_PrintString("Welcome to E4357 Course\r\n");
+		UART1_PrintString("Do you want to continue? Y/N\r\n");
+		databyte = UART1_Getchar();
+
+		if (databyte == 89)	//89 is ASCII for Y
+		{
+			UART1_PrintString("Y\r\n");
+			UART1_PrintString("Thank you and you have a good day :)\r\n");
+		}
+		else				//something else then Y
+		{
+			UART1_PrintString("N\r\n");
+			UART1_PrintString("Well then not :(\r\n");
+		}
+
+		while(1) //never ending loop
+		{
+		}
+
+}
